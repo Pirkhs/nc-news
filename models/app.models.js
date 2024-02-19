2dc35ea86ab998c30cb082e981632a9c89389f73
Pirkhs <nathan.briers32@gmail.com>
2024-02-19 16:31:33 +0000

    complete getting comments by article_id

--- models/app.models.js
+++ models/app.models.js
@@ -38,3 +38,16 @@ exports.selectArticleById = (article_id) => {
 //         return articlesToSend
 //     })
 // }
+
+
+exports.selectCommentsByArticleId = (article_id) => {
+    return db.query(`
+    SELECT * FROM comments 
+    WHERE article_id = $1
+    ORDER BY created_at DESC
+    `, [article_id])
+    .then(results => {
+        if (results.rows.length === 0) return Promise.reject({status: 404, msg: "Not found"})
+        return results.rows
+    })
+}
