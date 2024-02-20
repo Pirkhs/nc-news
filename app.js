13c8c78d15922545c2cbfafa73892c2ef2782316
Pirkhs <nathan.briers32@gmail.com>
2024-02-20 10:18:14 +0000

    complete POST_/api/articles/:article_id/comments

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-const {getAllTopics, getAllEndpoints, getArticleById, getAllArticles, getCommentsByArticleId} = require("./controllers/app.controllers.js")
+const {getAllTopics, getAllEndpoints, getArticleById, getAllArticles, getCommentsByArticleId, postCommentByArticleId} = require("./controllers/app.controllers.js")
+
+app.use(express.json())
 
 app.get("/api/topics", getAllTopics)
 app.get("/api", getAllEndpoints)
@@ -9,10 +11,11 @@ app.get("/api/articles/:article_id", getArticleById)
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId)
 
 
+app.post("/api/articles/:article_id/comments", postCommentByArticleId)
 
 // PSQL Errors
 app.use((err, request, response, next) => {
-    if (err.code === "22P02") response.status(400).send({msg: "Bad request"})
+    if (err.code === "22P02" || err.code === "23502") response.status(400).send({msg: "Bad request"})
     else next(err)
 })
 
