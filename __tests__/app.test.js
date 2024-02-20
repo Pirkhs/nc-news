74bcecaeb80202f5ee6ce301a6dac29842505e8e
Pirkhs <nathan.briers32@gmail.com>
2024-02-20 10:27:05 +0000

    refactored tests for getting article by ID so that the result is deconstructed in the model

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -47,14 +47,14 @@ describe('GET /api/articles:article_id', () => {
         .expect(200)
         .then(result => {
             const {article} = result.body
-            expect(article[0].article_id).toBe(1)
-            expect(article[0].title).toBe("Living in the shadow of a great man")
-            expect(article[0].topic).toBe("mitch")
-            expect(article[0].author).toBe("butter_bridge")
-            expect(article[0].body).toBe('I find this existence challenging')
-            expect(article[0].created_at).toBe("2020-07-09T20:11:00.000Z")
-            expect(article[0].votes).toBe(100)
-            expect(article[0].article_img_url).toBe("https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700")
+            expect(article.article_id).toBe(1)
+            expect(article.title).toBe("Living in the shadow of a great man")
+            expect(article.topic).toBe("mitch")
+            expect(article.author).toBe("butter_bridge")
+            expect(article.body).toBe('I find this existence challenging')
+            expect(article.created_at).toBe("2020-07-09T20:11:00.000Z")
+            expect(article.votes).toBe(100)
+            expect(article.article_img_url).toBe("https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700")
         })   
     });
 
--- models/app.models.js
+++ models/app.models.js
@@ -20,7 +20,7 @@ exports.selectArticleById = (article_id) => {
     return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
     .then(article => {
         if (article.rows.length === 0) return Promise.reject({status: 404, msg: "Not found"})
-        return article.rows
+        return article.rows[0]
     })
 }
 
