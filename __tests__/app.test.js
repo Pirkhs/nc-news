6396239a3eb2cc7f55dfe587839ce2b1bf1df10e
Pirkhs <nathan.briers32@gmail.com>
2024-02-21 14:58:40 +0000

    reworked getting articles by id to include comment_count in the response

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -55,6 +55,7 @@ describe('GET /api/articles:article_id', () => {
             expect(article.created_at).toBe("2020-07-09T20:11:00.000Z")
             expect(article.votes).toBe(100)
             expect(article.article_img_url).toBe("https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700")
+            expect(article.comment_count).toBe("11")
         })   
     });
 
