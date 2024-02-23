45fa9a1c17abf6de04ec189956bf5624d37a0b50
Pirkhs <nathan.briers32@gmail.com>
2024-02-23 10:14:49 +0000

    refactor connection.js slighlty

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
+const pool = new Pool()
 const ENV = process.env.NODE_ENV || 'development';
+const pathToEnvFile = `${__dirname}/../.env.${ENV}`
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
+require('dotenv').config({ path: pathToEnvFile });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
--- listen.js
+++ listen.js
@@ -1,5 +1,4 @@
 const app = require("./app.js")
 const { PORT = 9090 } = process.env;
 
-console.log(PORT)
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`))
